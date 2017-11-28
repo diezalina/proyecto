e1c4270f3983b7ba3a81294d53721c1c73cb70bc
ErikaItzelC <carrillo.lopez.itzel@gmail.com>
2017-11-28 17:53:54 -0600

    Revert "Merge branch 'testing' of https://github.com/diezalina/proyecto into testing"
    This reverts commit a4054972610c377e159095b2a2b777a374b0293f, reversing
    changes made to 2629d09dc28e42d69ca460a78dbe22e87e88eb54.

--- registrar.php
+++ registrar.php
@@ -1,6 +1,5 @@
 <?php
     try {
-<<<<<<< HEAD
         require_once('funciones/conexion.php');
         $nombre=$_GET['nombre'];
         $email=$_GET['email'];
@@ -10,16 +9,6 @@
         $sql="INSERT INTO
         comentarios(nombre,email,tipo,texto)";
         $sql .="values('{$nombre}','{$email}','{$tipo}','{$texto}');";
-=======
-        require_once(funciones/conexion.php);
-        $nombre=$_GET['nombre'];
-        $email=$_GET['email'];
-        $consulta=$_GET['consulta'];
-       
-        $sql="INSERT INTO
-        comentarios(nombre,email,consulta)";
-        $sql .="values('{$nombre}','{$email}','{$consulta}');";
->>>>>>> e1a2e72fb84c1de25d3b7875b6acbf582d7d2d0d
         $resultado=$conn->query($sql);
         }
     catch(Exception $e){
@@ -27,16 +16,9 @@
     }
 ?>
     
-<<<<<<< HEAD
 <html>
     <head>
         <link rel="stylesheet" href="css/estilo.css">
-=======
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/estilos.css">
->>>>>>> e1a2e72fb84c1de25d3b7875b6acbf582d7d2d0d
     </head>
     <body>
         <div class="bloque">
@@ -45,11 +27,8 @@
     if ($resultado){
         echo "Correcto";
     }
-<<<<<<< HEAD
             else
                 echo 'Error '.$error;
-=======
->>>>>>> e1a2e72fb84c1de25d3b7875b6acbf582d7d2d0d
     ?>
         </div>
     </body>
